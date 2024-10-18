60c041c96dc445586428cbd9f0263efbab9334fc
Ahad <shirohige@yonko.local>
2024-10-18 21:01:12 +0530

    resolving error

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ program
 program.command('count')
   .description('number of words in a file')
   .argument('<file>', 'file to count')
-  .action(file) => {
-    fs.readFile(file, 'utf-8', (err,data) => {
+  .action((file) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
       if (err) {
-<<<<<<< HEAD
         console.error(err);
         return;
       } else {
@@ -22,43 +21,8 @@ program.command('count')
           if (data[i] === ' ') {
             words++;
           }
-=======
-      console.log("error")
-    } else {
-      let words = 0
-      for(i = 0, i <= data.length, i++) {
-        if(data[i] === ' ') {
-        words++
->>>>>>> parent of 5f4af29 (word counter)
         }
-      }
-      console.log(`There are ${words +1} words in ${file}`)
-    }
-  })
-  }
-
-<<<<<<< HEAD
-program.command('count-sentences')
-  .description('Count the number of sentences in a file')
-  .argument('<file>', 'file to count')
-  .action((file) => {
-    fs.readFile(file, 'utf-8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      } else {
-        let sentences = 0;
-        for (let i = 0; i < data.length; i++) {
-<<<<<<< HEAD
-          if (data[i] === '.' || data[i] === '!' || data[i] === '?') {
-            sentences++;
-=======
-          if (data[i] === ' ') {
-            words++;
->>>>>>> origin/main
-          }
-        }
-        console.log(`There are ${sentences} sentences in ${file}`);
+        console.log(`There are ${words + 1} words in ${file}`);
       }
     });
   });
@@ -84,6 +48,3 @@ program.command('count-sentences')
   });
 
 program.parse();
-=======
-  program.parse();
->>>>>>> parent of 5f4af29 (word counter)
